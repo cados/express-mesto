28b2d6decec5d6c71b11a71a4edef59d43801a17
Kochkin Artem <tomsun86@gmail.com>
2021-01-14 14:23:38 +0300

    pr-12 Третье  ревью

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,10 +2,8 @@ const path = require('path');
 const getDataCards = require('../utils/readJsonFromFile');
 
 const cardsDataPath = path.join(__dirname, '../data/cards.json');
-const getCards = (req, res) =>
-  // eslint-disable-next-line implicit-arrow-linebreak
-  getDataCards(cardsDataPath)
-    .then((cards) => res.status(200).send(cards))
-    .catch((err) => res.status(500).send(err));
+const getCards = (req, res) => getDataCards(cardsDataPath)
+  .then((cards) => res.status(200).send(cards))
+  .catch((err) => res.status(500).send(err));
 
 module.exports = { getCards };
--- utils/readJsonFromFile.js
+++ utils/readJsonFromFile.js
@@ -1,23 +1,20 @@
+/* eslint-disable no-throw-literal */
 const fs = require('fs').promises;
 
-const getFileData = (pathToFile) =>
-  // eslint-disable-next-line implicit-arrow-linebreak
-  fs
-    .readFile(pathToFile)
-    .catch(() => {
-      // eslint-disable-next-line no-throw-literal
-      throw `file ${pathToFile} not found`;
-    })
-    .then((data) => {
-      try {
-        return JSON.parse(data);
-      } catch (_) {
-        // eslint-disable-next-line no-throw-literal
-        throw 'json is not valid';
-      }
-    })
-    .catch((err) => {
-      throw err;
-    });
+const getFileData = (pathToFile) => fs
+  .readFile(pathToFile)
+  .catch(() => {
+    throw ({ message: `Файл ${pathToFile} не найден` });
+  })
+  .then((data) => {
+    try {
+      return JSON.parse(data);
+    } catch (_) {
+      throw ({ message: 'json не валидный' });
+    }
+  })
+  .catch((err) => {
+    throw err;
+  });
 
 module.exports = getFileData;
